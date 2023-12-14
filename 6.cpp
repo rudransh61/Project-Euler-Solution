d874e09f96b3a68c99e85cfd8a860a82c98f0362
Rudransh Bhardwaj <aarti19830@gmail.com>
2023-12-14 19:11:29 +0530

    6 7

---
+++ 6.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n =100;
+    int ans = (3*n*n*n*n + 2*n*n*n  - 3*n*n  -2*n)/12 ;
+    cout<<ans<<endl;
+    
+
+    // ans : = 25164150
+
+    cout<<ans<<endl;
+}
---
+++ 7.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// initializing the max value
+#define MAX_SIZE 1000005
+ 
+// Function to generate N prime numbers using
+// Sieve of Eratosthenes
+void SieveOfEratosthenes(vector<int>& primes)
+{
+    // Create a boolean array "IsPrime[0..MAX_SIZE]" and
+    // initialize all entries it as true. A value in
+    // IsPrime[i] will finally be false if i is
+    // Not a IsPrime, else true.
+    bool IsPrime[MAX_SIZE];
+    memset(IsPrime, true, sizeof(IsPrime));
+ 
+    for (int p = 2; p * p < MAX_SIZE; p++) {
+        // If IsPrime[p] is not changed, then it is a prime
+        if (IsPrime[p] == true) {
+            // Update all multiples of p greater than or
+            // equal to the square of it
+            // numbers which are multiple of p and are
+            // less than p^2 are already been marked.
+            for (int i = p * p; i < MAX_SIZE; i += p)
+                IsPrime[i] = false;
+        }
+    }
+ 
+    // Store all prime numbers
+    for (int p = 2; p < MAX_SIZE; p++)
+        if (IsPrime[p])
+            primes.push_back(p);
+}
+ 
+// Driver Code
+int main()
+{
+    // To store all prime numbers
+    vector<int> primes;
+ 
+    // Function call
+    SieveOfEratosthenes(primes);
+ 
+    cout << "10001th prime number is " << primes[10000] << endl;
+    //ans : 104743
+    return 0;
+}
