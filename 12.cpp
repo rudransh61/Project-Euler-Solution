36c50db6b192e18accb2ca85ea7823fdb95fae00
Rudransh Bhardwaj <aarti19830@gmail.com>
2023-12-16 18:03:03 +0530

    12

--- 12.cpp
+++ 12.cpp
@@ -1,38 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-long long int numofdiv(long long int num){
-
-    long long int ans = 0;
-
-    for(long long int i=1ll; i<=num; i++){
-        if(num%i == 0){
-            ans ++;
+int countDivisors(int n) 
+{ 
+    int cnt = 0; 
+    for (int i = 1; i*i <= n; i++) { 
+        if (n % i == 0) { 
+            // If divisors are equal, 
+            // count only one 
+            if (n / i == i) 
+                cnt++; 
+  
+            else // Otherwise count both 
+                cnt = cnt + 2; 
+        } 
+    } 
+    return cnt; 
+} 
+int main()
+{
+    int n = 1;
+    while (true)
+    {
+        int triangleNumber = n * (n + 1) / 2;
+        if (countDivisors(triangleNumber) > 500)
+        {
+            cout << "The first triangle number with over 500 divisors is: " << triangleNumber << endl;
+            break; // exit the loop when condition is met
         }
+        ++n;
     }
 
-    return ans;
 
-    
-}
+    //ans  : 76576500
 
-int main(){
-
-    int ans = 0;
-
-    long long int n = 1;
-
-    for(long i=0;;i++){
-        n += i;
-        int divs  = numofdiv(n);
-        if(divs>500){
-            cout<<n<<endl;
-            break;
-        }
-    }
-
-    cout<<n<<endl;
-
-
-
+    return 0;
 }
